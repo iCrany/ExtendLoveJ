d0eb19e31c1fd058c472e4ab82c3b3a6817032df
iCrany <crany1992@gmail.com>
2014-07-09 09:07:32 +0800

    文章评论控制器的修改

---
+++ Extend_loveJ（test）/src/com/icrany/controller/blog/ArticleCommentController.java
@@ -0,0 +1,122 @@
+package com.icrany.controller.blog;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.directwebremoting.util.Logger;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.icrany.pojo.Article;
+import com.icrany.pojo.Comment;
+import com.icrany.pojo.Pager;
+import com.icrany.pojo.User;
+import com.icrany.service.ArticleService;
+import com.icrany.service.CategoryService;
+import com.icrany.service.CommentService;
+import com.icrany.service.LinkService;
+import com.icrany.service.SiteConfigService;
+import com.icrany.service.TagService;
+import com.icrany.service.UserService;
+import com.icrany.service.imp.ArticleServiceImp;
+import com.icrany.service.imp.CategoryServiceImp;
+import com.icrany.service.imp.CommentServiceImp;
+import com.icrany.service.imp.LinkServiceImp;
+import com.icrany.service.imp.SiteConfigServiceImp;
+import com.icrany.service.imp.TagServiceImp;
+import com.icrany.service.imp.UserServiceImp;
+
+@Controller
+@RequestMapping(value="/jsp/blog/")
+public class ArticleCommentController {
+	
+	private static final Logger logger = Logger.getLogger(ArticleCommentController.class);
+	
+	private static final String VIEW_BLOG = "/jsp/blog/blog_view";
+	
+	private static UserService userService = new UserServiceImp();
+	
+	private static SiteConfigService siteConfigService = new SiteConfigServiceImp();
+	
+	private static LinkService linkService = new LinkServiceImp();
+	
+	private static CommentService commentService = new CommentServiceImp();
+	
+	private static ArticleService articleService = new ArticleServiceImp();
+	
+	private static CategoryService categoryService = new CategoryServiceImp();
+	
+	private static TagService tagService = new TagServiceImp();
+	
+	/**
+	 * 这个类是用来处理 类型之间的转换的问题，如 date --> String 之间的转换
+	 * @param binder
+	 */
+	@InitBinder
+	private void dateBinder(WebDataBinder binder) {
+	            //The date format to parse or output your dates
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	            //Create a new CustomDateEditor
+	    CustomDateEditor editor = new CustomDateEditor(dateFormat, true);
+	            //Register it as custom editor for the Date type
+	    binder.registerCustomEditor(Date.class, editor);
+	}
+	
+	/**
+	 * 用来添加文章的评论用的
+	 * @param map
+	 * @param request
+	 * @param comment
+	 * @return
+	 */
+	@RequestMapping(value="/addComment")
+	public String addComment(Map<String,Object> map,HttpServletRequest request,Comment comment){
+		
+		System.out.println("name = " + comment.getName());
+		System.out.println("email = " + comment.getEmail());
+		System.out.println("site = " + comment.getSite());
+		System.out.println("content = " + comment.getContent());
+		System.out.println("articleId = " + comment.getArticleId());
+
+		Date postTime = new Date();//发表的时间
+		String postIP = null;//TODO:发表的 ip 地址
+		comment.setPostIP(postIP);
+		comment.setPostTime(postTime);
+		System.out.println("postTime = " + comment.getPostTime());
+		
+		commentService.insert(comment);
+		createReferenceData(map,comment.getArticleId());
+		
+		return  "redirect:" + VIEW_BLOG;
+	}
+	
+	//准备视图相关的一些数据
+	private void createReferenceData(Map<String,Object> map,int articleId){
+		User user = new User();
+		map.put("user", userService.find(user));//默认是只有一个用户的，这个参数是没有起作用的
+		map.put("tags", tagService.getAllTag());
+		map.put("categorys", categoryService.findAllCategory());
+		map.put("siteConfig", siteConfigService.findAllSiteConfig());//默认只有一个站点信息
+		map.put("article", articleService.queryById(articleId));
+		
+		//TODO:还要存放这篇文章下的所有评论
+		map.put("comments", commentService.findByArticleId(articleId));
+		
+		//TODO:最新的五篇文章
+		map.put("newestArticle", articleService.findNewestArticle());
+		
+		//TODO:最新的五篇评论
+		map.put("newestComment",commentService.findNewestComment());
+		
+		//TODO:最新的五个友情链接
+		map.put("newestLink", linkService.findNewestLink());
+	}	
+
+}
